92e1be28eba38f657c78ede96424ce4cad3d0040
ohdu <lidaryl@163.com>
2024-02-01 11:23:08 +0800

    feat: 文件内容新增关键字高亮

--- src/main/kotlin/page/home/widget/EditorWidget.kt
+++ src/main/kotlin/page/home/widget/EditorWidget.kt
@@ -1,18 +1,20 @@
 package page.home.widget
 
-import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.awt.SwingPanel
 import androidx.compose.ui.unit.dp
-import org.jetbrains.jewel.ui.component.Text
+import org.fife.ui.rsyntaxtextarea.FileLocation
+import org.fife.ui.rsyntaxtextarea.TextEditorPane
+import org.fife.ui.rtextarea.RTextScrollPane
+import util.extToSyntaxConstants
+import java.awt.BorderLayout
 import java.io.File
+import javax.swing.JPanel
+
 
 /**
  * @Author : lcc
@@ -22,19 +24,39 @@ import java.io.File
 @Composable
 fun EditorWidget(file: File) {
     Box(Modifier.fillMaxSize().padding(12.dp)) {
-        val scrollState = rememberLazyListState()
-        LazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            state = scrollState
-        ) {
-            file.forEachLine {
-                item { Text(it) }
-            }
-        }
+//        val scrollState = rememberLazyListState()
+//        LazyColumn(
+//            modifier = Modifier.fillMaxSize(),
+//            state = scrollState
+//        ) {
+//            file.forEachLine {
+//                item {
+//
+//                }
+//            }
+//        }
+//
+//        VerticalScrollbar(
+//            rememberScrollbarAdapter(scrollState),
+//            Modifier.align(Alignment.CenterEnd)
+//        )
 
-        VerticalScrollbar(
-            rememberScrollbarAdapter(scrollState),
-            Modifier.align(Alignment.CenterEnd)
-        )
+        SwingPanel(modifier = Modifier.fillMaxSize(), factory = {
+            JPanel(BorderLayout()).apply {
+                val textArea = TextEditorPane()
+                    .apply {
+                        load(FileLocation.create(file))
+                        syntaxEditingStyle = file.extToSyntaxConstants
+                        // 可折叠
+                        isCodeFoldingEnabled = true
+                        markOccurrences = true
+                        isMarginLineEnabled = true
+
+                    }
+                val sp = RTextScrollPane(textArea)
+                add(sp)
+            }
+        })
     }
 }
+
--- src/main/kotlin/util/FileUtil.kt
+++ src/main/kotlin/util/FileUtil.kt
@@ -1,5 +1,7 @@
 package util
 
+import org.fife.ui.rsyntaxtextarea.SyntaxConstants
+import java.io.File
 import java.text.DecimalFormat
 import kotlin.math.log10
 import kotlin.math.pow
@@ -26,3 +28,26 @@ fun Long.readableFileSize(): String {
 //    decimalFormat.setRoundingMode(RoundingMode.DOWN)
     return decimalFormat.format(this / 1024.0.pow(digitGroups.toDouble())) + units[digitGroups]
 }
+
+
+val File?.extToSyntaxConstants: String
+    get() = when (this?.extension) {
+        "c" -> SyntaxConstants.SYNTAX_STYLE_C
+        "cpp" -> SyntaxConstants.SYNTAX_STYLE_CPLUSPLUS
+        "css" -> SyntaxConstants.SYNTAX_STYLE_CSS
+        "csv" -> SyntaxConstants.SYNTAX_STYLE_CSV
+        "dart" -> SyntaxConstants.SYNTAX_STYLE_DART
+        "go" -> SyntaxConstants.SYNTAX_STYLE_GO
+        "groovy" -> SyntaxConstants.SYNTAX_STYLE_GROOVY
+        "hosts" -> SyntaxConstants.SYNTAX_STYLE_HOSTS
+        "java" -> SyntaxConstants.SYNTAX_STYLE_JAVA
+        "json" -> SyntaxConstants.SYNTAX_STYLE_JSON
+        "jsp" -> SyntaxConstants.SYNTAX_STYLE_JSP
+        "js" -> SyntaxConstants.SYNTAX_STYLE_JAVASCRIPT
+        "kt" -> SyntaxConstants.SYNTAX_STYLE_KOTLIN
+        "md" -> SyntaxConstants.SYNTAX_STYLE_MARKDOWN
+        "sql" -> SyntaxConstants.SYNTAX_STYLE_SQL
+        "xml" -> SyntaxConstants.SYNTAX_STYLE_XML
+        "html" -> SyntaxConstants.SYNTAX_STYLE_HTML
+        else -> SyntaxConstants.SYNTAX_STYLE_NONE
+    }
