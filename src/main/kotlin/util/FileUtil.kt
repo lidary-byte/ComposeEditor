0fd89387722dfa4c7b23ea80b67d52e033f49e79
ohdu <lidaryl@163.com>
2024-02-02 15:25:39 +0800

    feat : 文件格式补全

--- src/main/kotlin/util/FileUtil.kt
+++ src/main/kotlin/util/FileUtil.kt
@@ -32,22 +32,61 @@ fun Long.readableFileSize(): String {
 
 val File?.extToSyntaxConstants: String
     get() = when (this?.extension) {
+        "as" -> SyntaxConstants.SYNTAX_STYLE_ACTIONSCRIPT
+        "ams" -> SyntaxConstants.SYNTAX_STYLE_ASSEMBLER_X86
+        "bbcode" -> SyntaxConstants.SYNTAX_STYLE_BBCODE
         "c" -> SyntaxConstants.SYNTAX_STYLE_C
+        "clj" -> SyntaxConstants.SYNTAX_STYLE_CLOJURE
         "cpp" -> SyntaxConstants.SYNTAX_STYLE_CPLUSPLUS
+        "cs" -> SyntaxConstants.SYNTAX_STYLE_CSHARP
         "css" -> SyntaxConstants.SYNTAX_STYLE_CSS
         "csv" -> SyntaxConstants.SYNTAX_STYLE_CSV
+        "d" -> SyntaxConstants.SYNTAX_STYLE_D
         "dart" -> SyntaxConstants.SYNTAX_STYLE_DART
+        "pas" -> SyntaxConstants.SYNTAX_STYLE_DELPHI
+        "dtd" -> SyntaxConstants.SYNTAX_STYLE_DTD
+        "f", "f90", "f95" -> SyntaxConstants.SYNTAX_STYLE_FORTRAN
         "go" -> SyntaxConstants.SYNTAX_STYLE_GO
         "groovy" -> SyntaxConstants.SYNTAX_STYLE_GROOVY
+        "hbs", "handlebars" -> SyntaxConstants.SYNTAX_STYLE_HANDLEBARS
         "hosts" -> SyntaxConstants.SYNTAX_STYLE_HOSTS
+        "htaccess " -> SyntaxConstants.SYNTAX_STYLE_HTACCESS
+        "html" -> SyntaxConstants.SYNTAX_STYLE_HTML
+        "ini" -> SyntaxConstants.SYNTAX_STYLE_INI
         "java" -> SyntaxConstants.SYNTAX_STYLE_JAVA
+        "js" -> SyntaxConstants.SYNTAX_STYLE_JAVASCRIPT
         "json" -> SyntaxConstants.SYNTAX_STYLE_JSON
         "jsp" -> SyntaxConstants.SYNTAX_STYLE_JSP
-        "js" -> SyntaxConstants.SYNTAX_STYLE_JAVASCRIPT
         "kt" -> SyntaxConstants.SYNTAX_STYLE_KOTLIN
-        "md" -> SyntaxConstants.SYNTAX_STYLE_MARKDOWN
+        "tex" -> SyntaxConstants.SYNTAX_STYLE_LATEX
+
+        "less" -> SyntaxConstants.SYNTAX_STYLE_LESS
+        "lisp", "cl", "lsp" -> SyntaxConstants.SYNTAX_STYLE_LISP
+        "lua" -> SyntaxConstants.SYNTAX_STYLE_LUA
+        "md", "markdown" -> SyntaxConstants.SYNTAX_STYLE_MARKDOWN
+        "mxml" -> SyntaxConstants.SYNTAX_STYLE_MXML
+        "nsi" -> SyntaxConstants.SYNTAX_STYLE_NSIS
+        "pl", "pm" -> SyntaxConstants.SYNTAX_STYLE_PERL
+        "php" -> SyntaxConstants.SYNTAX_STYLE_PHP
+        "proto" -> SyntaxConstants.SYNTAX_STYLE_PROTO
+
+        "properties" -> SyntaxConstants.SYNTAX_STYLE_PROPERTIES_FILE
+        "py" -> SyntaxConstants.SYNTAX_STYLE_PYTHON
+        "rb" -> SyntaxConstants.SYNTAX_STYLE_RUBY
+        "sas" -> SyntaxConstants.SYNTAX_STYLE_SAS
+        "scala" -> SyntaxConstants.SYNTAX_STYLE_SCALA
         "sql" -> SyntaxConstants.SYNTAX_STYLE_SQL
+        "tcl" -> SyntaxConstants.SYNTAX_STYLE_TCL
+        "ts" -> SyntaxConstants.SYNTAX_STYLE_TYPESCRIPT
+        "vb" -> SyntaxConstants.SYNTAX_STYLE_VISUAL_BASIC
+        "bat" -> SyntaxConstants.SYNTAX_STYLE_WINDOWS_BATCH
         "xml" -> SyntaxConstants.SYNTAX_STYLE_XML
-        "html" -> SyntaxConstants.SYNTAX_STYLE_HTML
-        else -> SyntaxConstants.SYNTAX_STYLE_NONE
+        "yaml", "yml" -> SyntaxConstants.SYNTAX_STYLE_YAML
+
+
+        else -> when (this?.name) {
+            "Dockerfile" -> SyntaxConstants.SYNTAX_STYLE_DOCKERFILE
+            "Makefile" -> SyntaxConstants.SYNTAX_STYLE_MAKEFILE
+            else -> SyntaxConstants.SYNTAX_STYLE_NONE
+        }
     }
